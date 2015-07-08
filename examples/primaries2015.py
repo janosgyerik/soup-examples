8d6f8a70c8e40228921a1d955104deac55f41ce1
Janos Gyerik <janos@kronos>
2015-07-09 00:15:18 +0200

    much cleaner and generalized

--- examples/primaries2015.py
+++ examples/primaries2015.py
@@ -6,9 +6,26 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 
-URL = 'http://codereview.stackexchange.com/election/1?tab=primary'
-URL = 'http://serverfault.com/election/4?tab=primary'
-URL_FORMAT = 'http://{}/election/{}?tab=primary'
+COM_URL_FORMAT = 'http://{}.com/election/{}?tab=primary'
+STANDARD_URL_FORMAT = 'http://{}.stackexchange.com/election/{}?tab=primary'
+
+SITES_INFO = [
+    (('cr', 'codereview'), STANDARD_URL_FORMAT, 'codereview'),
+    (('sf', 'serverfault'), COM_URL_FORMAT, 'serverfault'),
+    (('so', 'stackoverflow'), COM_URL_FORMAT, 'stackoverflow'),
+]
+
+SITES = {}
+
+
+def init_sites():
+    for info in SITES_INFO:
+        names, url_format, url_component = info
+        for name in names:
+            SITES[name] = url_format, url_component
+
+
+init_sites()
 
 
 def load_html_doc(url):
@@ -23,10 +40,18 @@ def get_soup(url):
 
 
 def main():
-    parser = ArgumentParser('asd')
+    parser = ArgumentParser(description='Show candidates of an SE election in sorted order')
+    parser.add_argument('site', choices=SITES.keys(),
+                        help='Site short name or abbreviation')
+    parser.add_argument('-n', type=int, default=1,
+                        help='Election number')
     args = parser.parse_args()
 
-    soup = get_soup(URL)
+    site_info = SITES[args.site]
+    url_format, url_component = site_info
+    url = url_format.format(url_component, args.n)
+
+    soup = get_soup(url)
     scores = []
     for tr in soup.find_all('tr'):
         if tr.find('td', attrs={'class': 'votecell'}):
